8badecff7b0e397d7d56555899f6dc6c8e0eaa0e
Mintah <yawmintah1@gmail.com>
2024-04-29 00:48:20 +0000

    worked on hashing passwords in user.js

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const {isEmail} = require('validator')
+const bcrypt = require('bcrypt')
 
 const Schema = mongoose.Schema;
 
@@ -18,6 +19,15 @@ const userSchema = new Schema({
     }
 });
 
+// function to hash passwd data before saving
+userSchema.pre('save', async function (next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password,salt);
+    next();
+});
+
 const User = mongoose.model('user', userSchema);
+
+
 
 module.exports = User
